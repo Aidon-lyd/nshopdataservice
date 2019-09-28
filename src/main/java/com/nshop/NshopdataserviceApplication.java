00967532e5df5621007ba27564348d36fa41f7fe
liyadong <775600040@qq.com>
2019-09-28 14:43:51 +0800

    first commit.

---
+++ src/main/java/com/nshop/NshopdataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NshopdataserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NshopdataserviceApplication.class, args);
+    }
+
+}
